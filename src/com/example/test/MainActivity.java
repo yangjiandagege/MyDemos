47b8905267df6cd26eecb2525f4f484aa7a46fb8
yj <yj@yj-PC.(none)>
2016-04-12 23:14:13 +0800

    rm some activitys

--- src/com/example/test/MainActivity.java
+++ src/com/example/test/MainActivity.java
@@ -1,21 +1,7 @@
 package com.example.test;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.StreamCorruptedException;
-
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
-import android.util.Log;
-
-import com.example.test.common.User;
-import com.example.test.utils.*;
 
 public class MainActivity extends PreferenceActivity{
 
